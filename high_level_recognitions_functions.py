91b743adef7e97cc1ac3b0c06f3e6169e0a14eca
david <david@david.com>
2022-10-04 09:51:14 +0200

    dead called

--- high_level_recognitions_functions.py
+++ high_level_recognitions_functions.py
@@ -65,34 +65,35 @@ class UserActions:
 
         should_find_lower_than_position = True
         for i in range(3):
-            try:
-                print(f'click_to_that_image_down_way() try {i}')
-                is_move: bool = actions.user.move_image_relative(template_path,
-                                                                 disambiguator,
-                                                                 xoffset=xoffset,
-                                                                 yoffset=yoffset,
-                                                                 region=None,
-                                                                 gray_comparison=gray_comparison,
-                                                                 should_notify_message_if_fail=False,
-                                                                 threshold=threshold,
-                                                                 should_find_lower_than_position=should_find_lower_than_position,
-                                                                 max_x_position=max_x_position)
+            # try:
+            print(f'click_to_that_image_down_way() try {i + 1}')
+            is_move: bool = actions.user.move_image_relative(template_path,
+                                                             disambiguator,
+                                                             xoffset=xoffset,
+                                                             yoffset=yoffset,
+                                                             region=None,
+                                                             gray_comparison=gray_comparison,
+                                                             should_notify_message_if_fail=False,
+                                                             threshold=threshold,
+                                                             should_find_lower_than_position=should_find_lower_than_position,
+                                                             max_x_position=max_x_position)
 
-                if is_move:
-                    actions.sleep(0.5)
-                    actions.mouse_click(0)
-                    return
-                else:
-                    # raise_exception_if_not_matching_image_problem(e)
-                    actions.user.mouse_scroll_down(scroll_down_amount)
-                    sleep(1)
-                    # should_find_lower_than_position = False
-            except RuntimeError as e:
-                raise_exception_if_not_matching_image_problem(e)
-            #     actions.user.mouse_scroll_down(scroll_down_amount)
-            #     sleep(1)
-            #     should_find_lower_than_position = False
-        raise RuntimeError(f'No match for image={template_path} after {i} tries')
+            if is_move:
+                actions.sleep(0.5)
+                actions.mouse_click(0)
+                return
+            else:
+                # raise_exception_if_not_matching_image_problem(e)
+                actions.user.mouse_scroll_down(scroll_down_amount)
+                sleep(1)
+                should_find_lower_than_position = False
+        # except RuntimeError as e:
+        # raise_exception_if_not_matching_image_problem(e)
+
+        #     actions.user.mouse_scroll_down(scroll_down_amount)
+        #     sleep(1)
+        #     should_find_lower_than_position = False
+        raise RuntimeError(f'No match for image={template_path} after {i + 1} tries')
 
 
 def raise_exception_if_not_matching_image_problem(e: RuntimeError):
